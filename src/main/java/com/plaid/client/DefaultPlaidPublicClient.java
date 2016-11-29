e36d14d0426bc26b87ffd105b2ca659a15e46b39
petalv <mail@petteralvsten.com>
2016-11-29 18:19:59 +0100

    Adding institution all, throwing unused endpoints

--- src/main/java/com/plaid/client/DefaultPlaidPublicClient.java
+++ src/main/java/com/plaid/client/DefaultPlaidPublicClient.java
@@ -4,12 +4,7 @@ import com.plaid.client.exception.PlaidClientsideException;
 import com.plaid.client.http.HttpDelegate;
 import com.plaid.client.http.HttpResponseWrapper;
 import com.plaid.client.http.PlaidHttpRequest;
-import com.plaid.client.request.MappingOptions;
-import com.plaid.client.response.CategoriesResponse;
-import com.plaid.client.response.Category;
-import com.plaid.client.response.Institution;
 import com.plaid.client.response.InstitutionsResponse;
-import com.plaid.client.response.LongTailInstitutionsResponse;
 import org.apache.commons.lang.StringUtils;
 
 public class DefaultPlaidPublicClient implements PlaidPublicClient {
@@ -20,30 +15,11 @@ public class DefaultPlaidPublicClient implements PlaidPublicClient {
     private String secret;
 
     @Override
-    public Object getEntity(String entityId) {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
-
-    @Override
-    public Object getInstitution(String institutionId) {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
-
-    @Override
-    public InstitutionsResponse getAllInstitutions() {
-        PlaidHttpRequest request = new PlaidHttpRequest("/institutions");
-        HttpResponseWrapper<Institution[]> response = httpDelegate.doGet(request, Institution[].class);
-        return new InstitutionsResponse(response.getResponseBody());
-    }
-
-    @Override
-    public LongTailInstitutionsResponse getAllLongTailInstitutions(Integer offset, Integer count) {
-        PlaidHttpRequest request = new PlaidHttpRequest("/institutions/longtail");
-
+    public InstitutionsResponse getAllInstitutions(Integer offset, Integer count) {
+        PlaidHttpRequest request = new PlaidHttpRequest("/institutions/all");
         if(StringUtils.isEmpty(clientId) || StringUtils.isEmpty(secret)) {
             throw new PlaidClientsideException("ClientId and Secret is required");
         }
-
         request.addParameter("client_id", clientId);
         request.addParameter("secret", secret);
 
@@ -54,28 +30,10 @@ public class DefaultPlaidPublicClient implements PlaidPublicClient {
         if(offset != null) {
             request.addParameter("offset", offset.toString());
         }
-
-        HttpResponseWrapper<LongTailInstitutionsResponse> response = httpDelegate.doPost(request, LongTailInstitutionsResponse.class);
+        HttpResponseWrapper<InstitutionsResponse> response = httpDelegate.doPost(request, InstitutionsResponse.class);
         return response.getResponseBody();
     }
 
-    @Override
-    public CategoriesResponse getAllCategories() {
-        PlaidHttpRequest request = new PlaidHttpRequest("/categories");
-        HttpResponseWrapper<Category[]> response = httpDelegate.doGet(request, Category[].class);
-        return new CategoriesResponse(response.getResponseBody());
-    }
-
-    @Override
-    public Object getCategory(String categoryId) {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
-
-    @Override
-    public Object getCategoriesByMapping(String mapping, MappingOptions options) {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
-    
     @Override
     public HttpDelegate getHttpDelegate() {
     	return httpDelegate;
@@ -111,7 +69,6 @@ public class DefaultPlaidPublicClient implements PlaidPublicClient {
 
             return client;
         }
-
     }
 
 }
--- src/test/java/com/plaid/client/PlaidPublicClientTest.java
+++ src/test/java/com/plaid/client/PlaidPublicClientTest.java
@@ -1,14 +1,11 @@
 package com.plaid.client;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import com.plaid.client.exception.PlaidClientsideException;
-import com.plaid.client.response.LongTailInstitutionsResponse;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.Before;
@@ -16,24 +13,18 @@ import org.junit.Test;
 
 import com.plaid.client.http.ApacheHttpClientHttpDelegate;
 import com.plaid.client.http.HttpDelegate;
-import com.plaid.client.response.CategoriesResponse;
 import com.plaid.client.response.Institution;
 import com.plaid.client.response.InstitutionsResponse;
 
 public class PlaidPublicClientTest {
     
     private CloseableHttpClient httpClient;
-    private HttpDelegate httpDelegate;
     private PlaidPublicClient plaidPublicClientWithoutCredentials;
     private PlaidPublicClient plaidPublicClientWithCredentials;
-    
-    //@Rule
-    //public WireMockRule wireMockRule = new WireMockRule(8089);
 
     @Before
     public  void setup() {
         httpClient = HttpClients.custom().disableContentCompression().build();
-        //httpDelegate = new ApacheHttpClientHttpDelegate("http://localhost:8089", httpClient);
         HttpDelegate httpDelegate = new ApacheHttpClientHttpDelegate("https://tartan.plaid.com", httpClient);
         plaidPublicClientWithoutCredentials = new DefaultPlaidPublicClient.Builder()
                 .withHttpDelegate(httpDelegate)
@@ -45,18 +36,12 @@ public class PlaidPublicClientTest {
                 .withSecret("test_secret")
                 .build();
     }
-    
-    @Test
-    public void testGetAllCategories() {
-        CategoriesResponse categoriesResponse = plaidPublicClientWithoutCredentials.getAllCategories();
-        assertTrue(categoriesResponse.getCategories().size() > 0);
-    }
 
     @Test
     public void testGetAllInstitutions() {
-        InstitutionsResponse instResponse = plaidPublicClientWithoutCredentials.getAllInstitutions();
+        InstitutionsResponse instResponse = plaidPublicClientWithCredentials.getAllInstitutions(0, 10);
         assertNotNull(instResponse);
-        Institution[] institutions = instResponse.getInstitutions();
+        Institution[] institutions = instResponse.getResults();
         Map<String, Institution> map = new HashMap<>();
         for (Institution institution : institutions) {
             map.put(institution.getName(), institution);
@@ -66,13 +51,7 @@ public class PlaidPublicClientTest {
     }
 
     @Test(expected = PlaidClientsideException.class)
-    public void testGetAllLongTailInstitutionsRequireCredentials() throws Exception {
-        plaidPublicClientWithoutCredentials.getAllLongTailInstitutions(0, 10);
-    }
-
-    @Test
-    public void testGetAllLongTailInstitutions() throws Exception {
-        LongTailInstitutionsResponse response = plaidPublicClientWithCredentials.getAllLongTailInstitutions(0, 10);
-        assertEquals(10, response.getResults().length);
+    public void testGetAllInstitutionsRequireCredentials() throws Exception {
+        plaidPublicClientWithoutCredentials.getAllInstitutions(0, 10);
     }
 }
